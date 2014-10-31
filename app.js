1b96576b3e456e7cdc6b82e87d22b4057edeb060
sergey <serge.dmitriev@gmail.com>
2014-10-31 22:37:47 +0700

    add export

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
 var fs = require('fs');
 
-var A = require('./applications/');
-
-var Extension = require('./extension');
-var Context = require('./context');
-var Dialplan = require('./dialplan');
+var D = require('./index');
+var A = D.Applications;
 
 
-var extension = new Extension('120');
+var extension = new D.Extension('130');
 extension.append(new A.Set('qw', '123'));
 extension.append(new A.Answer(10));
 extension.append(new A.Verbose('additional'));
 extension.append('dial', new A.Dial('SIP/100'));
 extension.append(new A.Playback('hello', 'noanswer'));
-extension.append(new A.GotoIf('$["${CALL_FROM_GROUP}"="TRUE"]','dial'));
+extension.append(new A.GotoIf('$["${CALL_FROM_GROUP}"="TRUE"]', 'dial'));
 extension.append(new A.Hangup());
 
-var extension2 = new Extension('240');
+var extension2 = new D.Extension('240');
 extension2.append(new A.Set('qw', '123'));
 extension2.append(new A.Answer(10));
 extension2.append(new A.Verbose('additional'));
 extension2.append(new A.AGI('/tmp/agi.sh'));
 
 
-var context1 = new Context('fider');
+var context1 = new D.Context('fider');
 context1.append(extension);
 context1.append(extension2);
 
-var context2 = new Context('fiber');
+var context2 = new D.Context('fiber');
 context2.append(extension);
 
-var dialplan = new Dialplan();
+var dialplan = new D.Dialplan();
 dialplan.append(context1);
 dialplan.append(context2);
 
---
+++ index.js
@@ -0,0 +1,4 @@
+module.exports.Dialplan = require('./dialplan');
+module.exports.Context = require('./context');
+module.exports.Extension = require('./extension');
+module.exports.Applications = require('./applications/index');
